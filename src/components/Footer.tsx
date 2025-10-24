c320cc1d3178586ab67eb13508e35d82da7b9e3a
Prince <pk.504b@gmail.com>
2025-10-24 15:58:53 +0200

    footer on mobile fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-4 text-right text-xs italic opacity-70 flex justify-between">
+    <footer className="mt-4 text-xs italic opacity-70 flex flex-col gap-2 md:flex-row justify-between">
       <a href="https://taillens.io/?aff=zmn621">
         Need Tailwind CSS in your browser?
       </a>
