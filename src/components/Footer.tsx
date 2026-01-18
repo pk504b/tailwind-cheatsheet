ac8dd04cdce96ee62d1955e4dc93591882e493ba
Prince <pk.504b@gmail.com>
2026-01-18 12:36:29 +0100

    add x link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,23 @@
+import { Tailwindcss } from "./Icons";
+
 export default function Footer() {
   return (
-    <footer className="mt-4 text-xs italic opacity-70 flex flex-col gap-2 md:flex-row justify-between">
-      <a href="https://taillens.io/?aff=zmn621" target="_blank">
+    <footer className="mt-4 text-xs opacity-70 flex flex-col gap-2 md:flex-row justify-between">
+      <a
+        href="https://taillens.io/?aff=zmn621"
+        target="_blank"
+        className="italic"
+      >
         Need Tailwind CSS in your browser?
       </a>
 
-      <a href="mailto:pk.504b@gmail.com">
-        Send me a Hi 👋 (or feedbacks, queries, collaborations)
+      <a
+        href="https://x.com/pk504b"
+        target="_blank"
+        className="flex items-center gap-1"
+      >
+        Made with <Tailwindcss /> by{" "}
+        <span className="font-semibold">Prince(@pk504b)</span>
       </a>
     </footer>
   );
